100e4d808e4cd0eea11ffffd1f33941ef9ad6e42
Oscar Dominguez <dominguez.celada@gmail.com>
2022-08-31 22:39:01 +0200

    feat: initial version

---
+++ script.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * fixes workflows in octokit in a programmatic way
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
